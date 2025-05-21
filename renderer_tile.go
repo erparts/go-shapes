6bf18b57abe336037839ec83bd87131df8782de0
tinne <tinne26k@gmail.com>
2025-05-21 16:41:30 +0200

    Add triangular tiling functions

--- renderer_tile.go
+++ renderer_tile.go
@@ -4,28 +4,41 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+func (r *Renderer) TileRectsGrid(target *ebiten.Image, outWidth, outHeight, inWidth, inHeight, xOffset, yOffset float32) {
+	useOffsets := (xOffset != 0 || yOffset != 0)
+	if useOffsets {
+		r.opts.Uniforms["Offsets"] = [2]float32{xOffset, yOffset}
+	}
+	r.setFlatCustomVAs(outWidth, outHeight, inWidth, inHeight)
+	ensureShaderTileRectsGridLoaded()
+	r.DrawShader(target, 0, 0, shaderTileRectsGrid)
+	if useOffsets {
+		clear(r.opts.Uniforms)
+	}
+}
+
 // TileDotsHex draws dots of the given radius distributed in a hexagonal
 // lattice. HorzSpacing should always be at least twice the radius.
 func (r *Renderer) TileDotsHex(target *ebiten.Image, radius, horzSpacing, xOffset, yOffset float32) {
-	bounds := target.Bounds()
-	minX, minY := float32(bounds.Min.X), float32(bounds.Min.Y)
-	maxX, maxY := float32(bounds.Max.X), float32(bounds.Max.Y)
-	r.setDstRectCoords(minX, minY, maxX, maxY)
 	r.setFlatCustomVAs(radius, horzSpacing, xOffset, yOffset)
-
-	// draw shader
 	ensureShaderTileDotsHexLoaded()
-	target.DrawTrianglesShader(r.vertices[:], r.indices[:], shaderTileDotsHex, &r.opts)
+	r.DrawShader(target, 0, 0, shaderTileDotsHex)
 }
 
 func (r *Renderer) TileDotsGrid(target *ebiten.Image, radius, spacing, xOffset, yOffset float32) {
-	bounds := target.Bounds()
-	minX, minY := float32(bounds.Min.X), float32(bounds.Min.Y)
-	maxX, maxY := float32(bounds.Max.X), float32(bounds.Max.Y)
-	r.setDstRectCoords(minX, minY, maxX, maxY)
 	r.setFlatCustomVAs(radius, spacing, xOffset, yOffset)
-
-	// draw shader
 	ensureShaderTileDotsGridLoaded()
-	target.DrawTrianglesShader(r.vertices[:], r.indices[:], shaderTileDotsGrid, &r.opts)
+	r.DrawShader(target, 0, 0, shaderTileDotsGrid)
+}
+
+func (r *Renderer) TileTriUpGrid(target *ebiten.Image, outTriBase, inTriBase, xOffset, yOffset float32) {
+	r.setFlatCustomVAs(xOffset, yOffset, outTriBase, inTriBase)
+	ensureShaderTileTriUpGridLoaded()
+	r.DrawShader(target, 0, 0, shaderTileTriUpGrid)
+}
+
+func (r *Renderer) TileTriHex(target *ebiten.Image, outTriBase, inTriBase, xOffset, yOffset float32) {
+	r.setFlatCustomVAs(xOffset, yOffset, outTriBase, inTriBase)
+	ensureShaderTileTriHexLoaded()
+	r.DrawShader(target, 0, 0, shaderTileTriHex)
 }
--- renderer_tile_test.go
+++ renderer_tile_test.go
@@ -7,7 +7,19 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-// go test -run ^TestTileDotsHex ./... -count 1
+// go test -run ^TestTileRectsGrid . -count 1
+func TestTileRectsGrid(t *testing.T) {
+	app := NewTestApp(func(canvas *ebiten.Image, ctx TestAppCtx) {
+		canvas.Fill(color.Black)
+		ctx.Renderer.SetColorF32(0.9, 0, 0, 1.0)
+		ctx.Renderer.TileRectsGrid(canvas, 32, 48, 24, 24, 0, 0)
+	})
+	if err := ebiten.RunGame(app); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// go test -run ^TestTileDotsHex . -count 1
 func TestTileDotsHex(t *testing.T) {
 	app := NewTestApp(func(canvas *ebiten.Image, ctx TestAppCtx) {
 		canvas.Fill(color.Black)
@@ -23,7 +35,7 @@ func TestTileDotsHex(t *testing.T) {
 	}
 }
 
-// go test -run ^TestTileDotsGrid ./... -count 1
+// go test -run ^TestTileDotsGrid . -count 1
 func TestTileDotsGrid(t *testing.T) {
 	app := NewTestApp(func(canvas *ebiten.Image, ctx TestAppCtx) {
 		canvas.Fill(color.Black)
@@ -32,6 +44,42 @@ func TestTileDotsGrid(t *testing.T) {
 
 		ctx.Renderer.SetColor(color.RGBA{32, 64, 250, 255})
 		ctx.Renderer.TileDotsGrid(canvas, 5, 24, 12, 12)
+	})
+	if err := ebiten.RunGame(app); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// go test -run ^TestTileTriUpGrid . -count 1
+func TestTileTriUpGrid(t *testing.T) {
+	const inSize = 24
+	app := NewTestApp(func(canvas *ebiten.Image, ctx TestAppCtx) {
+		canvas.Fill(color.Black)
+		// ctx.Renderer.SetColorF32(0, 0.4, 0, 0.4)
+		// ctx.Renderer.TileDotsGrid(canvas, inSize/2, 32, 0, 0)
+
+		ctx.Renderer.SetColorF32(0, 0.3, 0.3, 0.5)
+		ctx.Renderer.TileRectsGrid(canvas, 32, 32*0.86602540378, inSize, inSize, 0, 0)
+
+		ctx.Renderer.SetColorF32(1.0, 0, 1.0, 1.0)
+		ctx.Renderer.TileTriUpGrid(canvas, 32, inSize, 0, 0)
+	})
+	if err := ebiten.RunGame(app); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// go test -run ^TestTileTriHex . -count 1
+func TestTileTriHex(t *testing.T) {
+	const minInSize = 12
+	const maxInSize = 30
+	app := NewTestApp(func(canvas *ebiten.Image, ctx TestAppCtx) {
+		canvas.Fill(color.Black)
+
+		xOff, yOff := float32(ctx.DistAnim(64, 0.5)), float32(ctx.DistAnim(32, 0.5))
+		dist := ctx.DistAnim(maxInSize-minInSize, 1.0)
+		ctx.Renderer.SetColorF32(1.0, 0, 1.0, 1.0)
+		ctx.Renderer.TileTriHex(canvas, 32, minInSize+float32(dist), xOff, yOff)
 	})
 	if err := ebiten.RunGame(app); err != nil {
 		t.Fatal(err)
